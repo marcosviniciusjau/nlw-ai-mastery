e64a62d6b20af8c80097e7657070eb4e0ecd4a92
Marcos Vinicius Araujo da Silva <marcosviniciusjahu@gmail.com>
2023-09-15 18:46:45 -0300

    requisições com ffmpeg

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -3,11 +3,13 @@ import { FileVideo, Upload } from "lucide-react"
 import { Textarea } from "./ui/textarea"
 import { Label } from "@radix-ui/react-label"
 import { Button } from "./ui/button"
-import { ChangeEvent, FormEvent, FormEventHandler, useMemo, useRef, useState } from "react"
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react"
+import { getFFmpeg } from "@/lib/ffmpeg"
+import {fetchFile} from '@ffmpeg/util'
 
 export function VideoInputForm(){
   const [videoFile, setVideoFile] = useState<File | null>(null)
-  
+
   const promptInputRef= useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>){
@@ -20,7 +22,45 @@ export function VideoInputForm(){
    setVideoFile(selectedFile)
 
   }
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+
+  async function convertVideoToAudio(video: File){
+    console.log('Converted started')
+
+    const ffmpeg= await getFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    //ffmpeg?.on('log', log)=>{
+      //console.log(log)
+    //}
+    ffmpeg.on('progress', progress=>{
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      ' 20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data= await ffmpeg.readFile('output.mp3')
+    
+    const audioFileBlob= new Blob([data], {type:'audio/npeg'})
+    const audioFile= new File([audioFileBlob],'audio.mp3',{
+      type:'audio/npeg',
+    })
+   console.log('Convert finished')
+   return audioFile
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
    const prompt= promptInputRef.current?.value
@@ -28,8 +68,11 @@ export function VideoInputForm(){
     return
    }
    
-
+  const audioFile= await convertVideoToAudio(videoFile)
+  console.log(audioFile)
+  
   }
+
   const previewURL= useMemo(()=>{
     if(!videoFile){
       return null
--- upload-ai-web/vite.config.ts
+++ upload-ai-web/vite.config.ts
@@ -5,9 +5,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+  fs: {
+    allow: ["./src", "./public", "C:/Users/Marcos Vinicius/node_modules/.pnpm/@ffmpeg+ffmpeg@0.12.6/node_modules/@ffmpeg/ffmpeg/dist/esm/worker.js"],
+    // ...
+  },
+  // ...
+
+    // ...
   },
 })
